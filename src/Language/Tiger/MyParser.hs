0b5a593f8ef7a8496b309b0d88171bcc23939c63
Matthew Chan <matt@themattchan.com>
2018-04-01 21:01:47 -0700

    myparser

---
+++ src/Language/Tiger/MyParser.hs
@@ -0,0 +1,5 @@
+-- | This module exists only to export the Alex/Happy generated code
+-- to the testing machinery.
+module Language.Tiger.MyParser (parseProgram) where
+
+import Language.Tiger.Parser (parseProgram)
