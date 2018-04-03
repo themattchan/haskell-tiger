de2a604d22c97a4ca5f3488dfd404a8691689c70
Matthew Chan <matt@themattchan.com>
2018-04-03 03:02:36 -0700

    done transExp, reorganise Semant.hs

--- src/Language/Tiger/Semant.hs
+++ src/Language/Tiger/Semant.hs
@@ -27,85 +27,10 @@ import Language.Tiger.Gensym
 import qualified Language.Tiger.Env as Env
 import qualified Language.Tiger.Symtab as Symtab
 
-data TransError
-  = NamedTypeMismatch Symbol (Either TyC Ty) Ty
-  | TypeMismatch Ty Ty
-  | TypeUndefined Symbol
-  | NoHOF
-  | UnboundVariable Symbol
-  | NoSuchField Symbol Symbol Ty
-  | BreakOutsideLoop
-
-instance Show TransError where
-  show = \case
-    NamedTypeMismatch symb expect actual ->
-      "Type mismatch: "
-      <> show symb <> " is a "
-      <> show actual <> " not a "
-      <> either show show expect
-    TypeMismatch expect actual ->
-      "Type mismatch: expected " <> show expect <> " got " <> show actual
-    TypeUndefined ty ->
-      "Undefined type: " <> show ty
-    NoHOF ->
-      "Higher order functions not implemented"
-    UnboundVariable v ->
-      "Unbound variable: " <> show v
-    NoSuchField fld r rt ->
-      "No field named " <> show fld <> " in record " <> show r <> " of type " <> show rt
-    BreakOutsideLoop ->
-      "break statement outside loop"
-
-newtype MultipleErrors = MultipleErrors
-  { runMultipleErrors :: Seq.Seq (Span TransError) }
-
--- | log an error and keep going
-nonfatal :: (HasSrcSpan a, MonadWriter MultipleErrors m)
-         => a -> TransError -> m ()
-nonfatal = tell . Seq.singleton . Span . sp
-
--- | log an error and halt
-fatal :: (HasSrcSpan a, MonadWriter MultipleErrors m, MonadError MultipleErrors m)
-      => a -> TransError -> m ()
-fatal ann err = do
-  nonfatal ann err
-  errs <- ask
-  throwError errs
-
--- | Variable environment
-type VEnv = Symtab.Symtab Env.EnvEntry
-
--- | Typing environment
-type TEnv = Symtab.Symtab Ty
-
-type CheckConstraints m a =
-  ( HasSrcSpan a
-  , MonadWriter MultipleErrors m
-  , MonadError MultipleErrors m
-  , MonadReader (VEnv, TEnv) m
-  )
-
-type CheckM = (ExceptT MultipleErrors -- can probably get rid of this...
-               (WriterT MultipleErrors
-                 (ReaderT (VEnv, TEnv)
-                   Identity)))
-
-runCheckM :: CheckM a -> Either MultipleErrors a
-runCheckM = fmap fst
-          . runIdentity
-          . runReaderT (Symtab.venv0, Symtab.tenv0)
-          . runWriterT
-          . runExceptT
-
-lookupV :: (MonadReader (VEnv,TEnv) m) => Symbol -> m (Maybe EnvEntry)
-lookupV n = Symtab.lookup n <$> asks fst
-
-lookupT :: (HasSrcSpan a, MonadWriter MultipleErrors m)
-        => TEnv -> Symbol -> a -> m (Maybe Ty)
-lookupT env t a = maybe (emitError a (TypeUndefined t)) pure $ Symtab.lookup t env
-
+--------------------------------------------------------------------------------
 transProg :: CheckConstraints m a
           -> Exp a -> m ()
+--------------------------------------------------------------------------------
 transProg e = do
   transExp e
   errs <- ask
@@ -113,9 +38,11 @@ transProg e = do
     then throwError errs
     else pure ()
 
+--------------------------------------------------------------------------------
 transVar :: CheckConstraints m a
          => VEnv -> TEnv
          -> Var a -> m (Var (Ty, a))
+--------------------------------------------------------------------------------
 transVar venv tenv = \case
   SimpleVar varName a ->
     Symtab.lookupV varName >>= \case
@@ -135,42 +62,21 @@ transVar venv tenv = \case
     do Var _ (ty, _)  <- transVar venv tenv recVar
         undefined
 
-annotTy :: Ty -> Exp a -> Exp (Ty, a)
-annotTy t e = (t, ) <$> e
-
-getTy :: Ann f => f (Ty, a) -> Ty
-getTy = fst . ann
-
-checkOp :: CheckConstraints m a -- (HasSrcSpan a)
-        => Op -> Ty -> Ty -> a
-        -> m Ty
-checkOp op tl tr a
-  | tl /= tr
-  = emitError a (TypeMismatch tl tr)
-  | isArith
-  = checks [tl, tr] IntTy
-
-  | isComp = undefined
-  | isEq = undefined
-  where
-    checks expect = (expect <$) . traverse (\x -> checkTy a x expect)
-
-    isArith = any (op ==) [Plus, Minus, Times, Divide]
-    isComp  = any (op ==) [Lt, Le, Gt, Ge]
-    isEq    = any (op ==) [Eq, Neq]
-
-checkTy :: HasSrcSpan a => a -> Ty -> Ty -> m ()
-checkTy expect declared
-  | expect == declared = pure ()
-  | otherwise = fatal ann (TypeMismatch expect declared)
-
+--------------------------------------------------------------------------------
 transExp :: (HasSrcSpan a, MonadWriter MultipleErrors m)
          => VEnv -> TEnv -> Int
          -> Exp a
          -> m (Exp (Ty, a))
+--------------------------------------------------------------------------------
 transExp venv tenv loopLevel exp =
   let
     go = transExp venv tenv loopLevel
+
+    goCheck e expectTy = do
+      e' <- go e
+      let (ty, a) = ann e'
+      checkTy a ty expectTy
+      return e'
 
     -- checkFieldTy fe fty = do
     --   fe' <- go fe
@@ -179,21 +85,31 @@ transExp venv tenv loopLevel exp =
     --   return fe'
 
   in case exp of
-    Var v ann
-      -> do v' <- transVar venv tenv v
-            pure $ Var v' (getTy v', ann)
+    Var v ann ->
+      do v' <- transVar venv tenv v
+         pure $ Var v' (getTy v', ann)
 
-    Nil _
-      -> annotTy NilTy exp
+    Nil _ ->
+      annotTy NilTy exp
 
-    Int _
-      -> annotTy IntTy exp
+    Int _ ->
+      annotTy IntTy exp
 
-    String s _
-      -> annotTy StringTy exp
+    String s _ ->
+      annotTy StringTy exp
 
-    Call fun args a ->
-      do <- lookupV venv fun
+    Call fun args ann ->
+      case Symtab.lookup venv fun of
+        Just (FunEntry argsTy returnTy)
+          | length args == length argsTy ->
+            do args' <- for (zip args argsTy) $ \(argE, argTy) ->
+                 do argE' <- goCheck argE argTy
+                    return argE'
+               return $ Call fun args' (returnTy, ann)
+          | otherwise ->
+            fatal ann (ArityError fun (length argsTy) (length args))
+
+        Nothing -> fatal ann (UndefinedFunction fun)
 
     Op l o r ann -> do
       l' <- go l
@@ -201,16 +117,31 @@ transExp venv tenv loopLevel exp =
       to <- checkOp o (getTy l') (getTy r')
       return $ Op l' o r' (to, ann)
 
--- FIXME
-    Record fields recTy _
-      -> lookupT recTy >>= \case
-           Just actualRecTy@(RecordTy fieldTys uid)
-             | length fields == length fieldTys ->
-                 do { zipWithM checkTy fields fieldTys
-                    ; annotTy actualRecTy exp
-                    }
-           Just _ -> throwError TypeMismatch
-           Nothing -> throwError TypeUndefined
+    Record fields recTySymb ann ->
+      case Symtab.lookup tenv recTySymb of
+        Just ty@(RecordTy fieldTys _) ->
+          | length fields == length fieldTys ->
+              do fields' <- for (zip fields fieldTys) $
+                    \((recFldName, recFldExp, recFldAnn),
+                        Field{fieldName, fieldType, fieldAnnot}) ->
+                       do recFldName == fieldName
+                          e' <- go recFldExp
+                          let fldType = fromJust $ Symtab.lookup tenv fieldType
+                           --  this should not fail
+                          checkTy recFldAnn (getTy e') fldType
+                          return (recFldName, e', (getTy e', recFldAnn))
+                 return $ Record fields' recTySymb (ty, ann)
+
+          | otherwise ->
+             fatal ann $
+               TypeMismatch recTySymb
+                   (Right ty)
+                   (RecordTy (map (\(x,y,_) -> (x,y)) fields) 99999)
+
+        -- recTySymb is not a record
+        Just ty -> fatal ann (TypeMismatch recTySymb (Left RecordTyC) ty)
+
+        Nothing -> nonfatal ann (TypeUndefined recTySymb)
 
     Seq seqs ann
       | null seqs ->
@@ -228,33 +159,28 @@ transExp venv tenv loopLevel exp =
          return $ Assign v' e' (UnitTy, ann)
 
     If c t mf ann ->
-      do c' <- go c
-         checkTy (getTy c') IntTy
+      do c' <- goCheck c IntTy
          t' <- go t'
          case mf of
            Nothing ->
              do checkTy (getTy t') UnitTy
-                return If c' t' Nothing (UnitTy, ann)
+                return $ If c' t' Nothing (UnitTy, ann)
 
            Just f ->
-             do f' <- go f
-                checkTy (getTy t') (getTy f')
+             do f' <- goCheck f (getTy t')
                 return $ If c' t' f' (getTy t', ann)
 
     While c b ann ->
-      do c' <- go c
-         checkTy (getTy c') IntTy
+      do c' <- goCheck c IntTy
          b' <- transExp venv tenv (loopLevel+1) b
-         checkTy (getTy b') UnitTy
+         checkTy (snd (ann b')) (getTy b') UnitTy
          return $ While c' b' (UnitTy, ann)
 
     For i lo hi b ann ->
-      do lo' <- go lo
-         checkTy (getTy lo') IntTy
-         hi' <- go hi
-         checkTy (getTy hi') IntTy
+      do lo' <- goCheck lo IntTy
+         hi' <- goCheck hi IntTy
          b' <- transExp (Symtab.insert i (VarEntry IntTy) venv) tenv (loopLevel+1)
-         checkTy (getTy b') UnitTy
+         checkTy (snd (ann b')) (getTy b') UnitTy
          return $ For i lo' hi' b' (UnitTy, ann)
 
     Break ann ->
@@ -262,28 +188,28 @@ transExp venv tenv loopLevel exp =
            nonfatal ann BreakOutsideLoop
          pure $ Break (UnitTy, ann)
 
-    Let binds e _
-      -> do (venv', tenv') <- foldM (uncurry transDec) (venv,tenv) binds
-            ty <- transExp venv' tenv' e
-            annotTy (getTy ty) exp
+    Let binds e _ ->
+      do (venv', tenv') <- foldM (uncurry transDec) (venv,tenv) binds
+         ty <- transExp venv' tenv' e
+         annotTy (getTy ty) exp
 
-    Array arrTySymb sizeE initE ann
-      -> case Symtab.lookup tenv arrTySymb of
-           Just ty@(ArrayTy elemTy _) -> do
-             sizeE' <- go sizeE
-             checkTy (getTy sizeE') IntTy
-             initE' <- go initE
-             checkTy (getTy initE') elemTy
-             return $ Array arrTySymb sizeE' initE' (ty, ann)
+    Array arrTySymb sizeE initE ann ->
+      case Symtab.lookup tenv arrTySymb of
+        Just ty@(ArrayTy elemTy _) -> do
+          sizeE' <- goCheck sizeE IntTy
+          initE' <- goCheck initE elemTy
+          return $ Array arrTySymb sizeE' initE' (ty, ann)
 
-           Just ty -> fatal ann (TypeMismatch arrTySymb (Left ArrayTyC) ty)
-           Nothing -> nonfatal ann (TypeUndefined arrTySymb) -- if nonfatal,
-                      -- should probably add this type into env and continue...
+        Just ty -> fatal ann (TypeMismatch arrTySymb (Left ArrayTyC) ty)
+        Nothing -> nonfatal ann (TypeUndefined arrTySymb) -- if nonfatal,
+                   -- should probably add this type into env and continue...
 
+--------------------------------------------------------------------------------
 transDec :: CheckConstraints m a
          => VEnv -> TEnv
          => Dec a
          -> m (VEnv, TEnv)
+--------------------------------------------------------------------------------
 transDec venv tenv dec = case dec of
   FunctionDecl fs a ->
     let doFun venv tenv Function{..} =
@@ -319,9 +245,11 @@ transDec venv tenv dec = case dec of
        let tenv' = Symtab.insert s ty tenv
        return (venv, tenv')
 
+--------------------------------------------------------------------------------
 transTy :: (Gensym m, MonadWriter MultipleErrors m)
         => TEnv
         -> AST.Ty a -> m Ty
+--------------------------------------------------------------------------------
 transTy tenv ty = case ty of
   NameTy symb maybeTy _
      -> undefined
@@ -329,3 +257,124 @@ transTy tenv ty = case ty of
      -> undefined
   ArrayTy symv uniq _
      -> undefined
+
+--------------------------------------------------------------------------------
+-- * Errors
+
+data TransError
+  = NamedTypeMismatch Symbol (Either TyC Ty) Ty
+  | TypeMismatch Ty Ty
+  | TypeUndefined Symbol
+  | ArityError Symbol Int Int
+  | NoHOF
+  | UndefinedFunction Symbol
+  | UnboundVariable Symbol
+  | NoSuchField Symbol Symbol Ty
+  | BreakOutsideLoop
+
+instance Show TransError where
+  show = \case
+    NamedTypeMismatch symb expect actual ->
+      "Type mismatch: "
+      <> show symb <> " is a "
+      <> show actual <> " not a "
+      <> either show show expect
+    TypeMismatch expect actual ->
+      "Type mismatch: expected " <> show expect <> " got " <> show actual
+    TypeUndefined ty ->
+      "Undefined type: " <> show ty
+    ArityError fn expect actual ->
+      show fn <> " called with the wrong number of arguments, expected "
+      <> show expect <> " got " <> show actual
+    NoHOF ->
+      "Higher order functions not implemented"
+    UndefinedFunction fn ->
+      "Undefined function: " <> show fn
+    UnboundVariable v ->
+      "Unbound variable: " <> show v
+    NoSuchField fld r rt ->
+      "No field named " <> show fld <> " in record " <> show r <> " of type " <> show rt
+    BreakOutsideLoop ->
+      "break statement outside loop"
+
+newtype MultipleErrors = MultipleErrors
+  { runMultipleErrors :: Seq.Seq (Span TransError) }
+
+-- | log an error and keep going
+nonfatal :: (HasSrcSpan a, MonadWriter MultipleErrors m)
+         => a -> TransError -> m ()
+nonfatal = tell . Seq.singleton . Span . sp
+
+-- | log an error and halt
+fatal :: (HasSrcSpan a, MonadWriter MultipleErrors m, MonadError MultipleErrors m)
+      => a -> TransError -> m ()
+fatal ann err = do
+  nonfatal ann err
+  errs <- ask
+  throwError errs
+
+--------------------------------------------------------------------------------
+-- * Monad
+
+-- | Variable environment
+type VEnv = Symtab.Symtab Env.EnvEntry
+
+-- | Typing environment
+type TEnv = Symtab.Symtab Ty
+
+type CheckConstraints m a =
+  ( HasSrcSpan a
+  , MonadWriter MultipleErrors m
+  , MonadError MultipleErrors m
+  , MonadReader (VEnv, TEnv) m
+  )
+
+type CheckM = (ExceptT MultipleErrors -- can probably get rid of this...
+               (WriterT MultipleErrors
+                 (ReaderT (VEnv, TEnv)
+                   Identity)))
+
+runCheckM :: CheckM a -> Either MultipleErrors a
+runCheckM = fmap fst
+          . runIdentity
+          . runReaderT (Symtab.venv0, Symtab.tenv0)
+          . runWriterT
+          . runExceptT
+
+-- * Helpers
+
+lookupV :: (MonadReader (VEnv,TEnv) m) => Symbol -> m (Maybe EnvEntry)
+lookupV n = Symtab.lookup n <$> asks fst
+
+lookupT :: (HasSrcSpan a, MonadWriter MultipleErrors m)
+        => TEnv -> Symbol -> a -> m (Maybe Ty)
+lookupT env t a = maybe (emitError a (TypeUndefined t)) pure $ Symtab.lookup t env
+
+annotTy :: Ty -> Exp a -> Exp (Ty, a)
+annotTy t e = (t, ) <$> e
+
+getTy :: Ann f => f (Ty, a) -> Ty
+getTy = fst . ann
+
+checkOp :: CheckConstraints m a -- (HasSrcSpan a)
+        => Op -> Ty -> Ty -> a
+        -> m Ty
+checkOp op tl tr a
+  | tl /= tr
+  = emitError a (TypeMismatch tl tr)
+  | isArith
+  = checks [tl, tr] IntTy
+
+  | isComp = undefined
+  | isEq = undefined
+  where
+    checks expect = (expect <$) . traverse (\x -> checkTy a x expect)
+
+    isArith = any (op ==) [Plus, Minus, Times, Divide]
+    isComp  = any (op ==) [Lt, Le, Gt, Ge]
+    isEq    = any (op ==) [Eq, Neq]
+
+checkTy :: HasSrcSpan a => a -> Ty -> Ty -> m ()
+checkTy ann expect declared
+  | expect == declared = pure ()
+  | otherwise = fatal ann (TypeMismatch expect declared)
